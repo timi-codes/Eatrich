74dd81bd09b87d9977a89a51929d2d16bb21a562
Tejumola David <tejumoladavid@gmail.com>
2017-04-07 12:43:11 +0100

    Update config.php

--- store/config.php
+++ store/config.php
@@ -19,9 +19,9 @@ define('DIR_UPLOAD', DIR_SYSTEM . 'storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('DB_USERNAME', 'smw73rsewyi7wok9');
-define('DB_PASSWORD', 'birmlt6gszy9g4de');
-define('DB_DATABASE', 'oh8u3g0ucp0zbidb');
+define('DB_HOSTNAME', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
